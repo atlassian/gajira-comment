8d9e5bd53febb3b6d09590b904fd2fa14b47a49a
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2021-12-21 11:09:09 -0600

    [KNOX-190]: Multiple Issue Comments and No Repeat Comments (#1)
    * add multiple issue comment support
    
    * build dist js
    
    * testing getIssue
    
    * print out sub fields and add if short circuit
    
    * fix comment check
    
    * allowRepeats input
    
    * print argv
    
    * parse inputs correctly
    
    * change for of loop to continue instead of return
    
    * - skip getIssue call if allowing repeats
    - add try-catch block for graceful errors
    
    * build

--- action.js
+++ action.js
@@ -1,25 +1,41 @@
-const Jira = require('./common/net/Jira')
+const Jira = require('./common/net/Jira');
 
 module.exports = class {
-  constructor ({ githubEvent, argv, config }) {
+  constructor({ githubEvent, argv, config }) {
     this.Jira = new Jira({
       baseUrl: config.baseUrl,
       token: config.token,
       email: config.email,
-    })
+    });
 
-    this.config = config
-    this.argv = argv
-    this.githubEvent = githubEvent
+    this.config = config;
+    this.argv = argv;
+    this.githubEvent = githubEvent;
   }
 
-  async execute () {
-    const issueId = this.argv.issue || this.config.issue || null
-    const { comment } = this.argv
+  async execute() {
+    const issues = this.argv.issue || this.config.issue || null;
+    const { comment, allowRepeats } = this.argv;
 
-    console.log(`Adding comment to ${issueId}: \n${comment}`)
-    await this.Jira.addComment(issueId, { body: comment })
+    const issueIds = typeof issues === 'string' ? issues.split(',') : [];
+    for (const issueId of issueIds) {
+      try {
+        if (allowRepeats === 'false') {
+          const resp = await this.Jira.getIssue(issueId);
 
-    return {}
+          if (resp.fields.comment.comments.find((commentObj) => commentObj.body === comment) !== undefined) {
+            console.log(`Comment already exists on issue: ${issueId}`);
+            continue;
+          }
+        }
+
+        console.log(`Adding comment to ${issueId}: \n${comment}`);
+        await this.Jira.addComment(issueId, { body: comment });
+      } catch (err) {
+        console.log(`Failed to add comment on ${issueId}:`, err);
+      }
+    }
+
+    return {};
   }
-}
+};
--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-const fs = require('fs')
-const YAML = require('yaml')
-const core = require('@actions/core')
+const fs = require('fs');
+const YAML = require('yaml');
+const core = require('@actions/core');
 
-const cliConfigPath = `${process.env.HOME}/.jira.d/config.yml`
-const configPath = `${process.env.HOME}/jira/config.yml`
-const Action = require('./action')
+const cliConfigPath = `${process.env.HOME}/.jira.d/config.yml`;
+const configPath = `${process.env.HOME}/jira/config.yml`;
+const Action = require('./action');
 
 // eslint-disable-next-line import/no-dynamic-require
-const githubEvent = require(process.env.GITHUB_EVENT_PATH)
-const config = YAML.parse(fs.readFileSync(configPath, 'utf8'))
+const githubEvent = require(process.env.GITHUB_EVENT_PATH);
+const config = YAML.parse(fs.readFileSync(configPath, 'utf8'));
 
-async function exec () {
+async function exec() {
   try {
     const result = await new Action({
       githubEvent,
       argv: parseArgs(),
       config,
-    }).execute()
+    }).execute();
 
     if (result) {
-      const extendedConfig = Object.assign({}, config, result)
+      const extendedConfig = Object.assign({}, config, result);
 
-      fs.writeFileSync(configPath, YAML.stringify(extendedConfig))
+      fs.writeFileSync(configPath, YAML.stringify(extendedConfig));
 
-      return
+      return;
     }
 
-    console.log('Failed to comment an issue.')
-    process.exit(78)
+    console.log('Failed to comment an issue.');
+    process.exit(78);
   } catch (error) {
-    console.error(error)
-    process.exit(1)
+    console.error(error);
+    process.exit(1);
   }
 }
 
-function parseArgs () {
+function parseArgs() {
   return {
     issue: core.getInput('issue'),
-    comment: core.getInput('comment')
-  }
+    comment: core.getInput('comment'),
+    allowRepeats: core.getInput('allow-repeats'),
+  };
 }
 
-exec()
+exec();
